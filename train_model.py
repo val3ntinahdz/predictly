488ec25479e6a9c730019869bbe5fc14813a396a
val3ntinahdz <valentinaleonh8@gmail.com>
2025-08-19 23:00:38 -0600

    add train_model.py script for preprocessing and training

--- train_model.py
+++ train_model.py
@@ -5,7 +5,14 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import joblib
 
-# load dataset
+# Cargar dataset
 iris = load_iris()
 x = pd.DataFrame(iris.data, columns=iris.feature_names)
 y = pd.Series(iris.target, name='species')
+
+# Preprocesamiento y división
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Entrenar el modelo
+model = RandomForestClassifier(random_state=42)
+model.fit(x_train, y_train)
